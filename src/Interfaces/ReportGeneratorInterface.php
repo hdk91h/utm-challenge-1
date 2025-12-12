88ba84b7de60b4b445d3e2a2f714ba288a8df504
hdk91h <29450358+hdk91h@users.noreply.github.com>
2025-12-12 12:30:38 +0100

    docs: add class description

--- src/Interfaces/ReportGeneratorInterface.php
+++ src/Interfaces/ReportGeneratorInterface.php
@@ -2,6 +2,9 @@
 
 namespace App\Interfaces;
 
+/**
+ * ReportGeneratorInterface entkoppelt die Logik, so lassen sich verschiedene Generatoren implementieren.
+ */
 interface ReportGeneratorInterface
 {
     public function generate(array $data, string $outputFile): void;
